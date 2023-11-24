4a0f2ea7fdea3f7cf2a4d4bfabdc3e01a19ae9e8
Ajay Kumar Gupta <rg190559@gmail.com>
2023-11-24 17:23:49 +0530

    adding sevices

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,33 @@
 import "./Services.css";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdSecurity } from "react-icons/md";
+import { GiReceiveMoney } from "react-icons/gi";
+import { RiSecurePaymentLine } from "react-icons/ri";
 
 function Services() {
   return (
-    <div>
-      <h1>this is services</h1>
+    <div className="services">
+      <h1>Our Services</h1>
+      <div className="card card-1">
+        <TbTruckDelivery className="icon" />
+        <h3>Super Fast and Free Delivery</h3>
+      </div>
+
+      <div className="card card-2">
+        <div className="card-colum-2">
+          <MdSecurity className="icon" />
+          <h3>Non-contact Shipping</h3>
+        </div>
+        <div className="card-colum-2">
+          <GiReceiveMoney className="icon" />
+          <h3>Money-back Guaranteed</h3>
+        </div>
+      </div>
+
+      <div className="card card-3">
+        <RiSecurePaymentLine className="icon" />
+        <h3>Super Secure Payment System</h3>
+      </div>
     </div>
   );
 }
