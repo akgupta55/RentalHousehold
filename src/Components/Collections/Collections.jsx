2a0ca662ce538192c816393ed844e373bac803d5
Ajay Kumar Gupta <rg190559@gmail.com>
2023-12-02 16:16:15 +0530

    changes in nav bar adding new components trading items

--- src/Components/Collections/Collections.jsx
+++ src/Components/Collections/Collections.jsx
@@ -1,11 +1,67 @@
 import "./Collections.css";
+import all_product from "../assets/all_product";
+import Item from "../Item/Item";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import NextArrow from "../NextArrow/NextArrow";
+import PrevArrow from "../PrevArrow/PrevArrow";
 
 function Collections() {
+  const settings = {
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="collections">
       <h1>Our Tranding Collections</h1>
-      <div className="container">
-        <h1>..</h1>
+      <div className="card">
+        <Slider {...settings}>
+          {all_product.map((data, index) => {
+            return (
+              <Item
+                key={index}
+                id={data.id}
+                image={data.image}
+                name={data.name}
+                new_price={data.new_price}
+                old_price={data.old_price}
+              />
+            );
+          })}
+        </Slider>
       </div>
     </div>
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import "./Home.css";
-import banner from "../../Components/assets/banner.png";
 import Services from "../../Components/Services/Services";
 import Categories from "../../Components/Categories/Categories";
 import Collections from "../../Components/Collections/Collections";
+import Banner from "../../Components/Banner/Banner";
 
 function Home() {
   return (
     <div className="home">
-      <img src={banner} alt="" />
+      <Banner />
       <Categories />
       <Collections />
       <Services />
